b05268276b79f3eb87e27285932c3fda464faa78
may Li <yaweel@me.com>
2013-08-25 12:55:40 +0800

    adding user input

--- hottest10.rb
+++ hottest10.rb
@@ -3,10 +3,17 @@ require 'soundcloud'
 
 client = Soundcloud.new(:client_id => '7be899ca72e6534d952737e40093afe8')
 
+page_size = 400
 
 puts "welcome to hottest10 !"
 
+
+
 loop do
+
+	print "now tell me a number from 1 to 15 in your mind :  "
+
+	page=Integer(gets. chomp) 
 
 	puts "would you like to check out the hottest tracks on soundcloud by genre or latest ?"
  
@@ -20,8 +27,8 @@ loop do
 
 		genre = gets.chomp
 
-		tracks = client.get('/tracks',  genres: "#{genre}" )   
-
+		tracks = client.get('/tracks',  genres: "#{genre}" , :limit => page_size, :offset => page_size*page)   
+	
 		tracks.delete_if{|t|t=!t.playback_count||t.playback_count==0 ||t.favoriting_count == 0}
 
 		tracks.delete_if{|t|t=t.comment_count.nil?}
@@ -32,27 +39,28 @@ loop do
 
 		newa=tracks.take(10) 
 
-		newa.each { |t| puts  t.permalink_url}
-
+		newa.each { |t| puts  t.permalink_url }
 
 	elsif decision=="latest"
 
-		page_size = 100
+		tracks = client.get('/tracks', :order => 'created_at', :bpm=>{:from=>80}, :limit => page_size,:offset => page_size*page)
+		
+		tracks.delete_if{|t|t=!t.playback_count||t.playback_count==0 ||t.favoriting_count == 0}
 
-		tracks = client.get('/tracks', :order => 'created_at', :limit => page_size, :offset => page_size)
+		tracks.delete_if{|t|t=t.comment_count.nil?}
 
-		tracks.delete_if {|t|t=!t.playback_count||t.playback_count==0}
+		tracks.delete_if{|t|t=t.download_count.nil?}
 
-		tracks.sort_by!{|t|- (t.playback_count+t.download_count+t.favoritings_count*2+t.comment_count*2)}
+		tracks.sort_by!{|t|(t.download_count+t.playback_count)/(t.download_count*0.5+t.favoritings_count+t.comment_count/0.5)}
 
 		newa=tracks.take(10) 
 
-		newa.each { |t| puts  t.permalink_url}
-
+		newa.each { |t| puts  t.permalink_url }
 
 	else  puts "wrong answer, what were you thinking" 
 
 	end
+	
 
 	print "would you like to keep searching ? (yes or no) :  "
 
